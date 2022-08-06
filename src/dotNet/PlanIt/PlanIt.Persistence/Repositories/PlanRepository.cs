77e16191ab6c16673ee5cb0e571fc4bcb2018dd6
alex casadio <alex.casadio92@gmail.com>
2022-08-06 10:06:52 +0200

    added user and plan repositories

---
+++ src/dotNet/PlanIt/PlanIt.Persistence/Repositories/PlanRepository.cs
@@ -0,0 +1,10 @@
+﻿using PlanIt.Application.Contracts.Persistence;
+using PlanIt.Domain.Entities;
+
+namespace PlanIt.Persistence.Repositories;
+internal class PlanRepository : BaseRepository<Plan>, IPlanrepository
+{
+    public PlanRepository(PlanItDbContext dbContext) : base(dbContext)
+    {
+    }
+}
---
+++ src/dotNet/PlanIt/PlanIt.Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,10 @@
+﻿using PlanIt.Application.Contracts.Persistence;
+using PlanIt.Domain.Entities;
+
+namespace PlanIt.Persistence.Repositories;
+public class UserRepository : BaseRepository<User>, IUserRepository
+{
+    public UserRepository(PlanItDbContext dbContext) : base(dbContext)
+    {
+    }
+}
