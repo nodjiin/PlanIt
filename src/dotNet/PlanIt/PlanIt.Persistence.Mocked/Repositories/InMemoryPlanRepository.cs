8882e4a0802ae4c62a8f9b74018d4ad0da03589c
alex casadio <alex.casadio92@gmail.com>
2022-08-06 11:30:34 +0200

    Added mocked persistence project

---
+++ src/dotNet/PlanIt/PlanIt.Persistence.Mocked/Repositories/InMemoryPlanRepository.cs
@@ -0,0 +1,36 @@
+﻿using PlanIt.Application.Contracts.Persistence;
+using PlanIt.Domain.Entities;
+
+namespace PlanIt.Persistence.Mocked.Repositories;
+public class InMemoryPlanRepository : IPlanrepository
+{
+    public Task<Plan> AddAsync(Plan entity, CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(Plan entity, CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Plan?> GetByIdAsync(Guid id, CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<Plan>> GetPagedResponseAsync(int page, int size, CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<Plan>> ListAllAsync(CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(Plan entity, CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/dotNet/PlanIt/PlanIt.Persistence.Mocked/Repositories/InMemoryUserRepository.cs
@@ -0,0 +1,36 @@
+﻿using PlanIt.Application.Contracts.Persistence;
+using PlanIt.Domain.Entities;
+
+namespace PlanIt.Persistence.Mocked.Repositories;
+public class InMemoryUserRepository : IUserRepository
+{
+    public Task<User> AddAsync(User entity, CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(User entity, CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User?> GetByIdAsync(Guid id, CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<User>> GetPagedResponseAsync(int page, int size, CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<User>> ListAllAsync(CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(User entity, CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+}
