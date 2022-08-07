907f6f17f66264b39e0ba68b8e1fd22ad878eef3
alex casadio <alex.casadio92@gmail.com>
2022-08-07 19:02:15 +0200

    implented in memory plan repository

--- src/dotNet/PlanIt/PlanIt.Persistence.Mocked/Repositories/InMemoryPlanRepository.cs
+++ src/dotNet/PlanIt/PlanIt.Persistence.Mocked/Repositories/InMemoryPlanRepository.cs
@@ -1,12 +1,13 @@
 ﻿using PlanIt.Application.Contracts.Persistence;
 using PlanIt.Domain.Entities;
+using System.Collections.Concurrent;
 
 namespace PlanIt.Persistence.Mocked.Repositories;
 public class InMemoryPlanRepository : IPlanRepository
 {
-    private readonly Dictionary<Guid, Plan> _plans = new();
-
-    public InMemoryPlanRepository()
+    private readonly ConcurrentDictionary<Guid, Plan> _plans = new();
+    private readonly IUserRepository _userRepository;
+    public InMemoryPlanRepository(IUserRepository userRepository)
     {
         Plan plan1 = new()
         {
@@ -17,19 +18,21 @@ public class InMemoryPlanRepository : IPlanRepository
             Id = Guid.NewGuid()
         };
 
-        _plans.Add(plan1.Id, plan1);
-        _plans.Add(plan2.Id, plan2);
+        _plans.TryAdd(plan1.Id, plan1);
+        _plans.TryAdd(plan2.Id, plan2);
+        _userRepository = userRepository;
     }
 
-    public Task<Plan> AddAsync(Plan entity, CancellationToken token = default)
+    public Task<Plan> AddAsync(Plan entity, CancellationToken token = default) => Task.Run(() =>
     {
-        throw new NotImplementedException();
-    }
+        _plans.TryAdd(entity.Id, entity);
+        return entity;
+    });
 
-    public Task DeleteAsync(Plan entity, CancellationToken token = default)
+    public Task DeleteAsync(Plan entity, CancellationToken token = default) => Task.Run(() =>
     {
-        throw new NotImplementedException();
-    }
+        _plans.TryRemove(entity.Id, out var _);
+    });
 
     public Task<Plan?> GetByIdAsync(Guid id, CancellationToken token = default)
     {
@@ -37,23 +40,28 @@ public class InMemoryPlanRepository : IPlanRepository
         return Task.FromResult(plan);
     }
 
-    public Task<Plan?> GetFullPlanByIdAsync(Guid id, CancellationToken token = default)
+    public async Task<Plan?> GetFullPlanByIdAsync(Guid id, CancellationToken token = default)
     {
-        throw new NotImplementedException();
+        _plans.TryGetValue(id, out var plan);
+        if (plan is null) return null;
+        plan.Users = (await _userRepository.ListAllAsync().ConfigureAwait(false)).Where(u => u.PlanId == id).ToList();
+        return plan;
     }
 
-    public Task<IReadOnlyList<Plan>> GetPagedResponseAsync(int page, int size, CancellationToken token = default)
+    public Task<IReadOnlyList<Plan>> GetPagedResponseAsync(int page, int size, CancellationToken token = default) => Task.Run(() =>
     {
-        throw new NotImplementedException();
-    }
+        return (IReadOnlyList<Plan>)_plans.Values.Skip((page - 1) * size).Take(size).ToList();
+    });
 
     public Task<IReadOnlyList<Plan>> ListAllAsync(CancellationToken token = default)
     {
         return Task.FromResult((IReadOnlyList<Plan>)_plans.Values.ToList());
     }
 
-    public Task UpdateAsync(Plan entity, CancellationToken token = default)
+    public Task UpdateAsync(Plan entity, CancellationToken token = default) => Task.Run(() =>
     {
-        throw new NotImplementedException();
-    }
+        _plans.TryGetValue(entity.Id, out var comparison);
+        if (comparison is null) return;
+        _plans.TryUpdate(entity.Id, entity, comparison);
+    });
 }
