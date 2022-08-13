cc7de71088f07f2b22dcc4c442805ba9474b45a8
alex casadio <alex.casadio92@gmail.com>
2022-08-13 22:49:52 +0200

    added Localization

--- src/dotNet/PlanIt/PlanIt.Presentation.WebApp/Program.cs
+++ src/dotNet/PlanIt/PlanIt.Presentation.WebApp/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+
+builder.Services
+    .AddLocalization(options => options.ResourcesPath = "Resources")
+    .AddControllersWithViews()
+    .AddMvcLocalization(LanguageViewLocationExpanderFormat.Suffix);
 
 var app = builder.Build();
 
@@ -13,6 +21,21 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+#region Localization
+var supportedCultures = new[]
+{
+    new CultureInfo("en"),
+    new CultureInfo("it"),
+};
+
+app.UseRequestLocalization(new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture("en"),
+    SupportedCultures = supportedCultures,
+    SupportedUICultures = supportedCultures
+});
+#endregion
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
