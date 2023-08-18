58bd6b475c0ccc4dea180aa01ac3bac2e532c687
alex casadio <alex.casadio92@gmail.com>
2023-08-18 22:17:39 +0800

    added cache to store old status values

--- src/dotNet/PlanIt/PlanIt.Presentation.WebApp/wwwroot/js/calendar.js
+++ src/dotNet/PlanIt/PlanIt.Presentation.WebApp/wwwroot/js/calendar.js
@@ -10,7 +10,8 @@ let nextMonthArr;
 // view data
 let currentUser;
 let currentMonth;
-let dateElements; // TODO I need to create multiple set of elements, based on the month
+let dateElements; 
+let statusCache = new Map();
 let fdMonth;
 
 // server side data
@@ -31,6 +32,9 @@ class DateElement {
     constructor(de) {
         this.#element = de;
         this.#element.addEventListener("click", (w) => this.#handleClick(w));
+    }
+    getStatus() {
+        return this.#status;
     }
     updateStatus(s) {
         switch (s) {
@@ -82,7 +86,7 @@ function handlePrevMonthArrowClick() {
     if (currentMonth <= minMonth) {
         return;
     }
-    updateMonth(--currentMonth);
+    updateMonth(currentMonth, --currentMonth);
     if (currentMonth === minMonth) {
         prevMonthArr.style.display = "none";
     }
@@ -93,7 +97,7 @@ function handleNextMonthArrowClick() {
     if (currentMonth >= maxMonth) {
         return;
     }
-    updateMonth(++currentMonth);
+    updateMonth(currentMonth, ++currentMonth);
     if (currentMonth === maxMonth) {
         nextMonthArr.style.display = "none";
     }
@@ -119,15 +123,27 @@ function getDaysInMonth(currentMonth) {
     return currentDate.getDate();
 }
 
-function updateMonth(number) {
-    if (!Number.isInteger(number) && number > 0 && number <= 12) {
-        console.error("Invalid month number value: ", number)
-        number = 0;
+function updateMonth(oldMonth, newMonth) {
+    if (!Number.isInteger(newMonth) && newMonth > 0 && newMonth <= 12) {
+        console.error("Invalid month number value: ", newMonth)
+        newMonth = 0;
     }
-    monthp.innerText = months[number]
+    monthp.innerText = months[newMonth]
+    if (oldMonth !== currentMonth) {
+        // TODO save old status
+        const statusesToCache = [35]
+        for (let i = 0; i < 35; i++) {
+            statusesToCache[i] = dateElements[i].getStatus();
+        }
+        statusCache.set(oldMonth, statusesToCache);
+    }
+    let cachedStatuses;
+    if (statusCache.has(newMonth)) {
+        cachedStatuses = statusCache.get(newMonth);
+    }
 
     // update the date elements based on the first date of the month 
-    const currentJsMonth = number - 1;
+    const currentJsMonth = newMonth - 1;
     fdMonth.setMonth(currentJsMonth);
     let firstDayOfWeek = fdMonth.getDay();
     if (firstDayOfWeek === 0) {
@@ -138,24 +154,41 @@ function updateMonth(number) {
 
     let dayIndex = 0;
     let el;
+    // TODO extract repetition
     while (dayIndex < firstDayOfWeek) {
-        el = dateElements[dayIndex++];
-        el.updateStatus(disabled);
+        el = dateElements[dayIndex];
+
+        if (cachedStatuses !== undefined) {
+            el.updateStatus(cachedStatuses[dayIndex]);
+        } else {
+            el.updateStatus(disabled);
+        }
         el.updateDayValue("-");
+        dayIndex++;
     }
 
     let lastValidDate = dayIndex + getDaysInMonth(currentJsMonth);
     let dayValue = 1;
     while (dayIndex < lastValidDate) {
-        el = dateElements[dayIndex++];
-        el.updateStatus(busy);
+        el = dateElements[dayIndex];
+        if (cachedStatuses !== undefined) {
+            el.updateStatus(cachedStatuses[dayIndex]);
+        } else {
+            el.updateStatus(busy);
+        }
         el.updateDayValue(dayValue++);
+        dayIndex++;
     }
 
     while (dayIndex < 35) {
-        el = dateElements[dayIndex++];
-        el.updateStatus(disabled);
+        el = dateElements[dayIndex];
+        if (cachedStatuses !== undefined) {
+            el.updateStatus(cachedStatuses[dayIndex]);
+        } else {
+            el.updateStatus(disabled);
+        }
         el.updateDayValue("-");
+        dayIndex++;
     }
 }
 
@@ -191,7 +224,7 @@ export function run(sMonths, sMinMonth, sMaxMonth, firstDayOfTheMonth) {
     fdMonth = firstDayOfTheMonth;
 
     // set current month and update arrow display
-    updateMonth(currentMonth);
+    updateMonth(currentMonth, currentMonth);
     prevMonthArr.style.display = "none";
     if (currentMonth === maxMonth) {
         nextMonthArr.style.display = "none";
